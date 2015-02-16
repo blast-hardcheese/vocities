fdab8e85ec61d835f17928e74150ae0b7c22bd23
Devon Stewart <blast@hardchee.se>
2015-02-16 02:17:18 -0800

    Display what domain we have determined we belong to

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,8 +7,8 @@ import play.api.Play.current
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index = BaseAction { request =>
+    Ok(request.domainId.toString)
   }
 
   def testData = BaseAction { request =>
