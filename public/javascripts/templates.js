64a73610c1538a4cee7fcedb9fd2d5cd7e6b9dfc
Devon Stewart <blast@hardchee.se>
2015-02-16 12:33:10 -0800

    Adding placeholder templates

---
+++ public/javascripts/templates.js
@@ -0,0 +1,8 @@
+var Templates = {
+    "0": React.createClass({
+        render: function() {
+            var div = React.createElement("span", null, "Hello, world!");
+            return React.createElement("div", null, div);
+        },
+    })
+}
