14026d41f5c780b761f959c135418bf77efdd328
Devon Stewart <blast@hardchee.se>
2015-02-16 02:17:08 -0800

    Create test data

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,6 +2,8 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.db.slick._
+import play.api.Play.current
 
 object Application extends Controller {
 
@@ -9,4 +11,10 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def testData = BaseAction { request =>
+    DB.withSession { implicit s =>
+      models.TestData.create()
+      Ok("Might have worked")
+    }
+  }
 }
