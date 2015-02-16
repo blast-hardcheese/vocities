2d604831ad847695d5253ed3cce9902ea7f874cb
Devon Stewart <blast@hardchee.se>
2015-02-16 02:17:39 -0800

    BaseController, BaseAction, and BaseRequest

---
+++ app/controllers/BaseController.scala
@@ -0,0 +1,30 @@
+package controllers
+
+import play.api.Logger
+import play.api.data.validation.ValidationError
+import play.api.libs.json._
+import play.api.mvc._
+import play.api.db.slick._
+import play.api.Play.current
+
+import scala.concurrent.Future
+
+trait BaseController extends Controller {
+  protected val log = Logger("application")
+  protected lazy val classLogger = Logger(this.getClass)
+}
+
+object BaseAction extends ActionBuilder[BaseRequest] with Results {
+  def invokeBlock[A](request: Request[A], block: (BaseRequest[A]) => Future[Result]): Future[Result] = {
+    val baseRequest = new BaseRequest[A](request)
+    block(baseRequest)
+  }
+}
+
+class BaseRequest[A](val request: Request[A]) extends WrappedRequest[A](request) {
+  lazy val domainId: Option[Long] = {
+    DB.withSession { implicit s =>
+      models.Domains.idFromFQDN(request.domain)
+    }
+  }
+}
