ec8016260dc1fd60df545092848114ff681652d0
Devon Stewart <blast@hardchee.se>
2015-03-14 18:51:32 -0700

    Preparing for prod config

--- app/Global.scala
+++ app/Global.scala
@@ -1,6 +1,8 @@
+import com.typesafe.config.ConfigFactory
+import java.io.File
 import play.api._
-import play.api.mvc._
 import play.api.mvc.Results._
+import play.api.mvc._
 import scala.concurrent.Future
 
 object LiftedRequestFilter extends Filter {
@@ -11,4 +13,9 @@ object LiftedRequestFilter extends Filter {
   }
 }
 
-object Global extends WithFilters(LiftedRequestFilter)
+object Global extends WithFilters(LiftedRequestFilter) {
+  override def onLoadConfig(config: Configuration, path: File, classloader: ClassLoader, mode: Mode.Mode): Configuration = {
+    val modeSpecificConfig = config ++ Configuration(ConfigFactory.load(s"application.${mode.toString.toLowerCase}.conf"))
+    super.onLoadConfig(modeSpecificConfig, path, classloader, mode)
+  }
+}
