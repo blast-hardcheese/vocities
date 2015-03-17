821504596d534200a15e102c1e01f6675007647e
Devon Stewart <blast@hardchee.se>
2015-03-16 21:43:46 -0700

    Adding initial account page

---
+++ app/controllers/Users.scala
@@ -0,0 +1,26 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.db.slick._
+import play.api.Play.current
+import play.api.libs.json.{ Json, JsValue }
+
+import play.twirl.api.Html
+
+import models.{ Accounts, Page, Template, UserModel, Queries }
+
+import securesocial.core.{ SecureSocial, RuntimeEnvironment }
+
+object Users extends BaseController {
+}
+
+class Users(override implicit val env: RuntimeEnvironment[UserModel]) extends BaseController with SecureSocial[UserModel] {
+
+  def index = SecuredAction { implicit request =>
+    DB.withSession { implicit s =>
+      val vm = Queries.accountsIndex(request.user.userId)
+      Ok(views.html.account.index(vm))
+    }
+  }
+}
