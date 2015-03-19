d4b9bd26dda5c756efe13d213fdb970a3fed592e
Devon Stewart <blast@hardchee.se>
2015-03-18 19:35:15 -0700

    Adding minimal editor interface

---
+++ app/assets/javascripts/editor.ts
@@ -0,0 +1,39 @@
+/// <reference path="dt/jquery/jquery.d.ts"/>
+
+interface PageInfo {
+    domain_id: number;
+    path: string;
+}
+
+declare var PageInfo: PageInfo;
+
+jQuery(function($) {
+    var $f = $('form'),
+        $textarea = $f.find('textarea'),
+        $save = $f.find('button[name=save]');
+
+    $textarea.val(JSON.stringify(JSON.parse($textarea.val()), undefined, '    '));
+
+    $save.click(function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        var data = JSON.parse($textarea.val());
+
+        var body = {
+            data: data,
+            title: $f.find('input[name=title]').val(),
+        };
+
+        $.ajax({
+            type: 'PUT',
+            contentType: 'application/json',
+            url: '/account/edit/' + PageInfo.domain_id + '/' + PageInfo.path,
+            data: JSON.stringify(body),
+        }).then(function() {
+                console.info('Success!');
+            }, function() {
+                console.info('Failure!');
+            });
+    });
+});
