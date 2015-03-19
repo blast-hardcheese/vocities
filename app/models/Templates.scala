0899b9013873c2ba4aed27fdc6935ed7302aa180
Devon Stewart <blast@hardchee.se>
2015-03-19 01:17:45 -0700

    Making CSS optional

--- app/models/Templates.scala
+++ app/models/Templates.scala
@@ -11,7 +11,7 @@ object TemplateData {
     case class Section(tag: String, title: String, content: Widget)
     case class Social(twitter: Option[String], facebook: Option[String], instagram: Option[String], github: Option[String], email: Option[String])
     case class Css(values: JsValue)
-    case class PageData(sections: List[Section], social: Social, sidebar: Sidebar, footer: Footer, css: Css)
+    case class PageData(sections: List[Section], social: Social, sidebar: Sidebar, footer: Footer, css: Option[Css])
 
     implicit val jsonFormatHeader = Json.format[Header]
     implicit val jsonFormatFooter = Json.format[Footer]
