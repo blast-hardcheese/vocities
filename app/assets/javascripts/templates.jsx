6741d4dba52a193c359a6bf0912357f3161d24f6
Devon Stewart <blast@hardchee.se>
2015-03-29 17:28:56 -0700

    Adding an edit mode for sidebar section titles

--- app/assets/javascripts/templates.jsx
+++ app/assets/javascripts/templates.jsx
@@ -105,9 +105,36 @@ var Templates = {
             componentDidUpdate: function() {
                 this.initScroller();
             },
+
+            renameSection: function(idx, data) {
+                var title = data.content;
+                var tag = title.toLowerCase().replace(/[^a-z0-9]+/g, '-');
+
+                this.props.updated({
+                    sections: _.map(this.props.sections, function(data, _idx) {
+                        if (_idx === idx) {
+                            data = _.extend({}, data, {
+                                title: title,
+                                tag: tag,
+                            });
+                        }
+                        return data;
+                    }),
+                });
+            },
+
             render: function() {
-                var sections = _.map(this.props.sections, function(s) {
-                    return <li key={s.tag}><a href={'#' + s.tag}>{s.title}</a></li>;
+                var _this = this;
+
+                var sections = _.map(this.props.sections, function(s, idx) {
+                    var link = null;
+                    if (_this.props.editing) {
+                        link = <TextField content={s.title} updated={_.partial(_this.renameSection, idx)} containerTag='a' href={'#' + s.tag} />;
+                    } else {
+                        link = <a href={'#' + s.tag}>{s.title}</a>;
+                    }
+
+                    return <li key={s.tag}>{link}</li>;
                 });
                 return (
                     <nav id="nav">
