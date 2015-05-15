f0fb8fe4d4747fb6246ad05228978ee0ebebde82
Devon Stewart <blast@hardchee.se>
2015-05-14 18:04:36 -0700

    Breaking out template widgets

--- app/assets/javascripts/templates.jsx
+++ app/assets/javascripts/templates.jsx
@@ -29,256 +29,256 @@ function TemplateManager(key, data) {
     };
 }
 
-var Templates = {
-    "html5up_read_only": (function(self) {
-        var Main = React.createClass({
-            mixins: [Utils, Editable, Updatable],
-            extendPropsFunctions: [Editable.extendPropsEditable, Updatable.autoUpdated],
+var Main = React.createClass({
+    mixins: [Utils, Editable, Updatable],
+    extendPropsFunctions: [Editable.extendPropsEditable, Updatable.autoUpdated],
 
-            componentDidMount: function() {
-                var _this = this;
-                this.setProps(_.extend({}, this.props, {
-                    updated: function(newProps) {
-                        _this.setProps(newProps);
-                    }
-                }));
-            },
-            render: function() {
-                var _this = this;
+    componentDidMount: function() {
+        var _this = this;
+        this.setProps(_.extend({}, this.props, {
+            updated: function(newProps) {
+                _this.setProps(newProps);
+            }
+        }));
+    },
+    render: function() {
+        var _this = this;
 
-                var sections = _.map(this.props.sections, function(s, idx) {
-                    return React.createElement(Section, _this.extendProps('sections.' + idx));
-                });
-                return (
-                    <div id="main">
-                        {sections}
-                    </div>
-                );
-            },
+        var sections = _.map(this.props.sections, function(s, idx) {
+            return React.createElement(Section, _this.extendProps('sections.' + idx));
         });
+        return (
+            <div id="main">
+                {sections}
+            </div>
+        );
+    },
+});
 
-        var ColorPicker = React.createClass({
-            getSchemes: function () {
-                var dynamicCssValues = JSON.parse($('#dynamic-tpl-values').text());
-                return dynamicCssValues['schemes'];
-            },
-            schemes: [
-                {
-                    "primary_bg": "#4acaa8",
-                    "primary_fg": "#d1f1e9",
-                    "secondary": "#b6e9dc",
-                    "accent": "#5ccfb0",
-                },
-                {
-                    "primary_bg": "#BD4ACA",
-                    "primary_fg": "#EDD1F1",
-                    "secondary": "#DEB6E9",
-                    "accent": "#C85CCF",
-                },
-            ],
-            update: function (data) {
-                this.props.updated(
-                    deepExtend("css.values", this.props, {
-                        "primary_bg": data.primary_bg,
-                        "primary_color": data.primary_fg,
-                        "hilight_color": data.secondary,
-                        "accent_color": data.accent,
-                        "nav_active_bg": "white",
-                        "nav_active_color": data.primary_bg,
-                    }));
-            },
-            select: function (idx) {
-                this.update(this.schemes[idx]);
-            },
-            render: function() {
-                var _this = this;
+var ColorPicker = React.createClass({
+    getSchemes: function () {
+        var dynamicCssValues = JSON.parse($('#dynamic-tpl-values').text());
+        return dynamicCssValues['schemes'];
+    },
+    schemes: [
+        {
+            "primary_bg": "#4acaa8",
+            "primary_fg": "#d1f1e9",
+            "secondary": "#b6e9dc",
+            "accent": "#5ccfb0",
+        },
+        {
+            "primary_bg": "#BD4ACA",
+            "primary_fg": "#EDD1F1",
+            "secondary": "#DEB6E9",
+            "accent": "#C85CCF",
+        },
+    ],
+    update: function (data) {
+        this.props.updated(
+            deepExtend("css.values", this.props, {
+                "primary_bg": data.primary_bg,
+                "primary_color": data.primary_fg,
+                "hilight_color": data.secondary,
+                "accent_color": data.accent,
+                "nav_active_bg": "white",
+                "nav_active_color": data.primary_bg,
+            }));
+    },
+    select: function (idx) {
+        this.update(this.schemes[idx]);
+    },
+    render: function() {
+        var _this = this;
 
-                var choices = this.schemes.map(function(o, idx) {
-                    var build = function (color) {
-                        return (
-                            <div style={{
-                                width: 20,
-                                height: 20,
-                                float: 'left',
-                                backgroundColor: color,
-                            }} />
-                        );
-                    };
-
-                    return (
-                        <div style={{
-                            marginTop: 5,
-                            border: '1px solid black',
-                            overflow: 'auto',
-                        }} key={idx} onClick={function() { return _this.select(idx); }}>
-                            {build(o.primary_bg)}
-                            {build(o.primary_fg)}
-                            {build(o.secondary)}
-                            {build(o.accent)}
-                        </div>
-                    );
-                });
-
+        var choices = this.schemes.map(function(o, idx) {
+            var build = function (color) {
                 return (
                     <div style={{
-                        position: 'fixed',
-                        top: 10,
-                        left: 10,
-                        zIndex: 1,
-                    }}>
-                        {choices}
-                    </div>
+                        width: 20,
+                        height: 20,
+                        float: 'left',
+                        backgroundColor: color,
+                    }} />
                 );
-            },
+            };
+
+            return (
+                <div style={{
+                    marginTop: 5,
+                    border: '1px solid black',
+                    overflow: 'auto',
+                }} key={idx} onClick={function() { return _this.select(idx); }}>
+                    {build(o.primary_bg)}
+                    {build(o.primary_fg)}
+                    {build(o.secondary)}
+                    {build(o.accent)}
+                </div>
+            );
         });
 
-        var EditButtons = React.createClass({
-            mixins: [Editable, Updatable],
+        return (
+            <div style={{
+                position: 'fixed',
+                top: 10,
+                left: 10,
+                zIndex: 1,
+            }}>
+                {choices}
+            </div>
+        );
+    },
+});
 
-            setEditable: function (newState) {
-                this.props.updated({
-                    editable: newState,
-                });
-            },
+var EditButtons = React.createClass({
+    mixins: [Editable, Updatable],
 
-            performSave: function () {
-                console.info('Save stub');
-                var pageData = _.extend({}, this.props);
+    setEditable: function (newState) {
+        this.props.updated({
+            editable: newState,
+        });
+    },
 
-                delete pageData.editable;
-                delete pageData.saveUrl;
-                delete pageData.title;
-                delete pageData.updated;
+    performSave: function () {
+        console.info('Save stub');
+        var pageData = _.extend({}, this.props);
 
-                var title = this.props.title;
+        delete pageData.editable;
+        delete pageData.saveUrl;
+        delete pageData.title;
+        delete pageData.updated;
 
-                var data = {
-                    title: title,
-                    data: pageData,
-                };
+        var title = this.props.title;
 
-                $.ajax({
-                    contentType: 'application/json',
-                    data: JSON.stringify(data),
-                    dataType: 'json',
-                    method: 'PUT',
-                    url: this.props.saveUrl,
-                });
-            },
+        var data = {
+            title: title,
+            data: pageData,
+        };
 
-            render: function() {
-                var toggleEditButton = null;
-                var saveButton = null;
+        $.ajax({
+            contentType: 'application/json',
+            data: JSON.stringify(data),
+            dataType: 'json',
+            method: 'PUT',
+            url: this.props.saveUrl,
+        });
+    },
 
-                var style = {
-                    position: 'fixed',
-                    top: 15,
-                    left: 97,
-                    zIndex: 1,
-                };
+    render: function() {
+        var toggleEditButton = null;
+        var saveButton = null;
 
-                var buttonStyle = {
-                    display: 'block',
-                };
+        var style = {
+            position: 'fixed',
+            top: 15,
+            left: 97,
+            zIndex: 1,
+        };
 
-                if (this.props.editable) {
-                    toggleEditButton = <button style={buttonStyle} onClick={this.setEditable.bind(this, false)}>Disable Editing</button>;
-                } else {
-                    toggleEditButton = <button style={buttonStyle} onClick={this.setEditable.bind(this, true)}>Enable Editing</button>;
-                }
+        var buttonStyle = {
+            display: 'block',
+        };
 
-                if (this.props.saveUrl) {
-                    saveButton = <button style={buttonStyle} onClick={this.performSave}>Save</button>;
-                }
+        if (this.props.editable) {
+            toggleEditButton = <button style={buttonStyle} onClick={this.setEditable.bind(this, false)}>Disable Editing</button>;
+        } else {
+            toggleEditButton = <button style={buttonStyle} onClick={this.setEditable.bind(this, true)}>Enable Editing</button>;
+        }
 
-                return <div style={style}>
-                    {toggleEditButton}
-                    {saveButton}
-                </div>;
+        if (this.props.saveUrl) {
+            saveButton = <button style={buttonStyle} onClick={this.performSave}>Save</button>;
+        }
+
+        return <div style={style}>
+            {toggleEditButton}
+            {saveButton}
+        </div>;
+    }
+});
+
+var AddWidgetPopup = React.createClass({
+    availableTypes: {
+        'paragraph': {
+            'class': Paragraph,
+            'name': 'Paragraph',
+        },
+
+        'youtube': {
+            'class': YouTube,
+            'name': 'YouTube Video',
+        },
+
+        'header': {
+            'class': HeaderBlock,
+            'name': 'Section Header',
+        },
+    },
+
+    addSection: function() {
+        var title = this.refs.sectionName.getDOMNode().value;
+        var type = this.refs.sectionType.getDOMNode().value;
+
+        var tag = title.toLowerCase().replace(/[^a-z0-9]+/g, '-');
+
+        var section = {
+            tag: tag,
+            title: title,
+            content: {
+                type: type,
             }
-        });
+        };
 
-        var AddWidgetPopup = React.createClass({
-            availableTypes: {
-                'paragraph': {
-                    'class': Paragraph,
-                    'name': 'Paragraph',
-                },
-
-                'youtube': {
-                    'class': YouTube,
-                    'name': 'YouTube Video',
-                },
-
-                'header': {
-                    'class': HeaderBlock,
-                    'name': 'Section Header',
-                },
-            },
-
-            addSection: function() {
-                var title = this.refs.sectionName.getDOMNode().value;
-                var type = this.refs.sectionType.getDOMNode().value;
-
-                var tag = title.toLowerCase().replace(/[^a-z0-9]+/g, '-');
-
-                var section = {
-                    tag: tag,
-                    title: title,
-                    content: {
-                        type: type,
-                    }
+        switch(type) {
+            case 'paragraph':
+                section.content.data = {
+                    content: '<p>Lorem ipsum sit dolor amet</p>',
                 };
+                break;
+            case 'youtube':
+                section.content.data = {
+                    videoId: 'vSjX02FIZCk',
+                };
+                break;
+            case 'header':
+                section.content.data = {
+                    title: title,
+                    text: '<p>Lorem ipsum sit dolor amet</p>',
+                    subtitle: '<p>Nunc convallis dictum consectetur</p>',
+                };
+                break;
+        }
 
-                switch(type) {
-                    case 'paragraph':
-                        section.content.data = {
-                            content: '<p>Lorem ipsum sit dolor amet</p>',
-                        };
-                        break;
-                    case 'youtube':
-                        section.content.data = {
-                            videoId: 'vSjX02FIZCk',
-                        };
-                        break;
-                    case 'header':
-                        section.content.data = {
-                            title: title,
-                            text: '<p>Lorem ipsum sit dolor amet</p>',
-                            subtitle: '<p>Nunc convallis dictum consectetur</p>',
-                        };
-                        break;
-                }
-
-                var data = _.extend({}, this.props, {
-                    sections: _.flatten([this.props.sections || [], [section]]),
-                });
-
-                this.props.updated(data);
-
-                $('#add-popup').hide();
-            },
-
-            render: function() {
-                var _this = this;
-
-                var options = _.map(Object.keys(this.availableTypes), function(kind) {
-                    return <option key={kind} value={kind}>{_this.availableTypes[kind].name}</option>;
-                });
-
-                return <div className="popup">
-                    <h2>Add Section</h2>
-                    <input ref="sectionName" className="name" placeholder="Enter section name here" />
-                    <select ref="sectionType">
-                        <option value="" disabled>Select widget type</option>
-                        {options}
-                    </select>
-
-                    <button onClick={this.addSection}>Add Section</button>
-                </div>;
-            },
+        var data = _.extend({}, this.props, {
+            sections: _.flatten([this.props.sections || [], [section]]),
         });
 
+        this.props.updated(data);
+
+        $('#add-popup').hide();
+    },
+
+    render: function() {
+        var _this = this;
+
+        var options = _.map(Object.keys(this.availableTypes), function(kind) {
+            return <option key={kind} value={kind}>{_this.availableTypes[kind].name}</option>;
+        });
+
+        return <div className="popup">
+            <h2>Add Section</h2>
+            <input ref="sectionName" className="name" placeholder="Enter section name here" />
+            <select ref="sectionType">
+                <option value="" disabled>Select widget type</option>
+                {options}
+            </select>
+
+            <button onClick={this.addSection}>Add Section</button>
+        </div>;
+    },
+});
+
+var Templates = {
+    "html5up_read_only": (function(self) {
         var render = function(sel, data, clazz) {
             var factory = React.createFactory(clazz)(data);
             var target = $(sel)[0];
