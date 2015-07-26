bd5f101a6fbe5cb4868ae27fd09ce52746a043e2
Devon Stewart <blast@hardchee.se>
2015-07-26 16:36:31 -0700

    Adding domain "class" to a domain

--- app/models/Domains.scala
+++ app/models/Domains.scala
@@ -1,19 +1,31 @@
 package models
 
 import utils.ExtendedPostgresDriver.simple._
+import utils.ExtendedPostgresDriver.createEnumJdbcType
+
+object DomainClasses extends Enumeration {
+  val Basic = Value("basic")
+  val Advanced = Value("advanced")
+  val Premium = Value("premium")
+  val Unlimited = Value("unlimited")
+
+  implicit val typeMapper = createEnumJdbcType("domain_class", DomainClasses)
+}
 
 case class Domain(
   id: Long,
   account_id: Long,
-  domain: String
+  domain: String,
+  domainClass: DomainClasses.Value = DomainClasses.Basic
 )
 
 class DomainTable(tag: Tag) extends Table[Domain](tag, "domains") {
   def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
   def account_id = column[Long]("account_id", O.NotNull)
   def domain = column[String]("domain", O.NotNull)
+  def domain_class = column[DomainClasses.Value]("class", O.NotNull)
 
-  def * = (id, account_id, domain) <> (Domain.tupled, Domain.unapply _)
+  def * = (id, account_id, domain, domain_class) <> (Domain.tupled, Domain.unapply _)
 }
 
 object Domains {
--- conf/evolutions/default/13.sql
+++ conf/evolutions/default/13.sql
@@ -1,7 +1,13 @@
 # --- !Ups
 
+CREATE TYPE domain_class AS ENUM ('basic', 'advanced', 'premium', 'unlimited');
+
 ALTER TABLE "accounts" ADD COLUMN "credits" int default 0;
+ALTER TABLE "domains" ADD COLUMN "class" domain_class default 'basic';
 
 # --- !Downs
 
+ALTER TABLE "domains" DROP COLUMN "class";
 ALTER TABLE "accounts" DROP COLUMN "credits";
+
+DROP TYPE domain_class;
