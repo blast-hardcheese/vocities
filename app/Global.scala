752e5240dd68cb86816ecdfb5cc12d6154784065
Devon Stewart <blast@hardchee.se>
2015-08-04 09:36:42 -1000

    Configurable IdentityProviders

--- app/Global.scala
+++ app/Global.scala
@@ -5,6 +5,7 @@ import play.api.mvc.Results._
 import play.api.mvc._
 import scala.concurrent.Future
 import scala.collection.immutable.ListMap
+import scala.collection.mutable.{ Set => MutableSet }
 import securesocial.core.providers.{ GoogleProvider, FacebookProvider, UsernamePasswordProvider }
 
 import java.lang.reflect.Constructor
@@ -13,6 +14,8 @@ import service.PostgresUserService
 import models.UserModel
 
 object Global extends WithFilters() {
+  var enabledProviders: MutableSet[String] = MutableSet.empty
+
   override def onLoadConfig(config: Configuration, path: File, classloader: ClassLoader, mode: Mode.Mode): Configuration = {
     import collection.JavaConversions._
 
@@ -31,6 +34,13 @@ object Global extends WithFilters() {
     super.onLoadConfig(modeSpecificConfig, path, classloader, mode)
   }
 
+  override def onStart(app: Application) {
+    val maybeProviders: Option[String] = app.configuration.getString("securesocial.providers")
+    val providers: Seq[String] = maybeProviders.map(_.split(',').toSeq).getOrElse(Seq.empty)
+
+    enabledProviders ++= providers
+  }
+
   /**
    * The runtime environment for this sample app.
    */
@@ -38,10 +48,15 @@ object Global extends WithFilters() {
     override implicit val executionContext = play.api.libs.concurrent.Execution.defaultContext
     override lazy val userService: PostgresUserService = new PostgresUserService()
 
-    override lazy val providers = ListMap(
+    private[this] def availableProviders: ListMap[String, securesocial.core.IdentityProvider] = ListMap(
       include(new GoogleProvider(routes, cacheService, oauth2ClientFor(GoogleProvider.Google))),
       include(new UsernamePasswordProvider[UserModel](userService, avatarService, viewTemplates, passwordHashers))
     )
+
+    override lazy val providers: ListMap[String, securesocial.core.IdentityProvider] = {
+      availableProviders
+        .filter(enabledProviders contains _._1)
+    }
   }
 
   /**
