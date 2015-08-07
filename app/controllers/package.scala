a1a83ec852dc89d836e4ff532eff8bf2cbcc7a85
Devon Stewart <blast@hardchee.se>
2015-08-06 15:52:31 -1000

    Adding With*Provider
    Extended securesocial example

--- app/controllers/package.scala
+++ app/controllers/package.scala
@@ -1,8 +1,23 @@
-import securesocial.core.SecureSocial
+import play.api.mvc.RequestHeader
+import securesocial.core.{ SecureSocial, Authorization }
 import models.UserModel
 
 package controllers {
   trait SecureController extends BaseController with SecureSocial[UserModel] {
     implicit val env = Global.MyRuntimeEnvironment
+  }
+
+  case class WithPrimaryProvider(provider: String) extends Authorization[UserModel] {
+    def isAuthorized(user: UserModel, request: RequestHeader) = {
+      user.main.providerId == provider
+    }
+  }
+
+  case class WithSecondaryProvider(provider: String) extends Authorization[UserModel] {
+    def isAuthorized(user: UserModel, request: RequestHeader) = {
+      user.identities.view
+        .filter(_.providerId != user.main.providerId)
+        .exists(_.providerId == provider)
+    }
   }
 }
