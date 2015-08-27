e8168cbdece86a7978e4963567723c46a6ef03f2
Devon Stewart <blast@hardchee.se>
2015-08-26 22:08:55 -0700

    Fixing role serialization issue

--- app/models/Users.scala
+++ app/models/Users.scala
@@ -3,21 +3,23 @@ package models
 import utils.ExtendedPostgresDriver.simple._
 import utils.ExtendedPostgresDriver.createEnumListJdbcType
 
-import UserRoles.{ UserRole, listTypeMapper }
-
 object UserRoles extends Enumeration {
   type UserRole = Value
   val Admin = Value("admin")
-
-  implicit val listTypeMapper = createEnumListJdbcType("userRole", UserRoles)
 }
 
-case class User(id: Long = -1, username: String, roles: List[UserRole] = List.empty)
+case class User(
+  id: Long = -1,
+  username: String,
+  roles: List[UserRoles.Value] = List.empty
+)
 
 class UserTable(tag: Tag) extends Table[User](tag, "users") {
+  implicit val listTypeMapper = createEnumListJdbcType("userRole", UserRoles)
+
   def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
-  def username = column[String]("username")
-  def roles = column[List[UserRole]]("roles")
+  def username = column[String]("username", O.NotNull)
+  def roles = column[List[UserRoles.Value]]("roles", O.NotNull)
 
   def * = (id, username, roles) <> (User.tupled, User.unapply)
 }
