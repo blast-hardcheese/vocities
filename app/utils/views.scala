a7a79ffd1f983bc62578e5a6396c2293b5d92952
Devon Stewart <blast@hardchee.se>
2015-08-29 16:49:40 -0700

    Refactoring out cloudinary settings

--- app/utils/views.scala
+++ app/utils/views.scala
@@ -17,5 +17,18 @@ package object views {
 
     Html(Json.stringify(newData).replace("</script>", "<\\/script>"))
   }
+
+  val cloudinaryData: Html = {
+    val config = play.api.Play.current.configuration
+    val maybeCloudinary = for {
+        cloud_name <- config.getString("cloudinary.cloud_name")
+        upload_preset <- config.getString("cloudinary.upload_preset")
+    } yield Json.obj(
+        "cloud_name" -> cloud_name,
+        "upload_preset" -> upload_preset
+    )
+
+    Html(Json.stringify(maybeCloudinary getOrElse Json.obj()))
+  }
 }
 
